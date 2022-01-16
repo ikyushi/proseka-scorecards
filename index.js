60c6fc607e6ed415ae477ad51613a2ca83f988cb
SimpleSenpai12 <70236472+simplesentai@users.noreply.github.com>
2022-01-16 18:45:42 -0500

    initial

---
+++ index.js
@@ -0,0 +1,5 @@
+const { createCanvas, loadImage } = require('canvas');
+const canvas = createCanvas(740, 740);
+const ctx = canvas.getContext('2d');
+
+// 4444ca is the color of the background for the difficulty number (that sounds really confusing)
